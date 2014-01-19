51864be05f23f45f1e785a822a6b6004daec77ec
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2014-01-19 18:23:52 +0900

    STIを使ってAPIを継承させた

---
+++ app/models/rakuten_api.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id              :integer          not null, primary key
+#  api_type        :string(255)      not null
+#  product_name    :string(255)      not null
+#  url             :string(255)      not null
+#  affiliate_url   :string(255)
+#  description     :text
+#  maker_name      :string(255)      not null
+#  price           :integer          default(0), not null
+#  last_updated_at :datetime         not null
+#  created_at      :datetime
+#  updated_at      :datetime
+#
+# Indexes
+#
+#  index_products_on_api_type  (api_type)
+#  index_products_on_price     (price)
+#
+
+class RakutenApi < Product
+end
